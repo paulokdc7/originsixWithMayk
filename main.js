88178bc88918140f35da4d2175985c4d942000a3
Paulomrtj <85956726+Paulomrtj@users.noreply.github.com>
2021-06-27 13:51:02 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,110 @@
+/* abre e fecha o menu quando clicar no icone */
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
+
+/* quando clicar em um item do menu, esconder o menu */
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
+
+/* adicionar uma sombra ao header */
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+function headerShadow() {
+  if (window.scrollY >= navHeight) {
+    // scroll é maior que a altura do header
+    header.classList.add('scroll')
+  } else {
+    // menor que a altura do header
+    header.classList.remove('scroll')
+  }
+}
+
+/* BACK TO TOP VISIBLE */
+const back = document.querySelector('.back-to-top')
+function backToTop() {
+  if (window.scrollY >= 180) {
+    back.classList.add('show')
+  } else {
+    back.classList.remove('show')
+  }
+}
+
+/* TESTIMONIALS CAROUSEL SWIPER SLIDER */
+const swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true,
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+      setWrapperSize: true
+    }
+  }
+})
+
+/* SCROLL REVEAL */
+const scrollReveal = ScrollReveal({
+  distance: '30px',
+  origin: 'top',
+  duration: 700,
+  reset: true
+})
+
+scrollReveal.reveal(
+  `#home .image, #home .text,
+  #about .image, #about .text,
+  #services header, #services .card,
+  #testimonials header, #testimonial .testimonal,
+  #contact .text, #contact .links,
+  footer .brand, footer .social
+  `,
+  { interval: 100 }
+)
+
+/* MENU ATIVO CONFORME A SESSÃO VISÍVEL NA PÁG */
+const sections = document.querySelectorAll('main section[id]')
+function activateMenuAtCurrentSection() {
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+
+  for (const section of sections) {
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionId = section.getAttribute('id')
+
+    const checkpointStart = checkpoint >= sectionTop
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+    if (checkpointStart && checkpointEnd) {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.add('active')
+    } else {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.remove('active')
+    }
+  }
+}
+
+/* WHEN SCROLL */
+
+window.addEventListener('scroll', function () {
+  headerShadow()
+  backToTop()
+  activateMenuAtCurrentSection()
+})
